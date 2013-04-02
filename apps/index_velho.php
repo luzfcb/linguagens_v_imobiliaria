cf35c249bd33ffb199175b65289599d98e749552
Fábio C. Barrionuevo da Luz <bnafta@gmail.com>
2013-04-01 21:43:03 -0300

    sofrendo com namespace parte 4, agora o sofrimento voltou com tudo

---
+++ apps/index_velho.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Criado por: Fabio C. Barrionuevo da Luz (github.com/luzfcb)
+ * Data: 29/03/13
+ * Horario: 13:12
+ */
+
+
+//Twig_Autoloader::register();
+// set autoload
+//spl_autoload_register(function ($class) {
+//    require_once(str_replace('\\', '/', $class . '.php'));
+//});
+use Imobiliaria\Models\TipoImovel;
+use tools\Twig_tool\TwigViewBase;
+
+$GLOBALS['APP_PATH'] = __DIR__;
+
+//use models\Contato;
+//use models\Endereco;
+//use models\Estado;
+//use models\Imagem;
+//use models\Imovel;
+//use models\Proprietario;
+//use models\TipoImovel;
+//use models\TipoNegocio;
+
+
+//$loader = new Twig_Loader_Filesystem('./templates');
+//$twig = new Twig_Environment($loader, array(
+//    'cache' => './tmp/cache',
+//));
+
+//$template = $twig->loadTemplate('hello.twig');
+$params = array(
+    'name' => 'Krzysztof',
+    'friends' => array(
+        array(
+            'firstname' => 'John',
+            'lastname' => 'Smith'
+        ),
+        array(
+            'firstname' => 'Britney',
+            'lastname' => 'Spears'
+        ),
+        array(
+            'firstname' => 'Brad',
+            'lastname' => 'Pitt'
+        )
+    )
+);
+//$template->display($params);
+
+function getClassAnnotations($class)
+{
+    $r = new ReflectionClass($class);
+    $doc = $r->getDocComment();
+    print_r($doc);
+    preg_match_all('#@(.*?)\n#s', $doc, $annotations);
+    return $annotations[1];
+}
+
+
+
+
+//require_once('models/TipoImovel.php');
+
+
+
+$tipo_imovel = new TipoImovel();
+$tipo_imovel->setDescricaoTipoImovel('Cobertura cara');
+
+
+//
+//$contato = new Contato('bnafta@gmail.com', '6392041028', '63');
+//
+//getClassAnnotations($contato);
+//$aa = $contato->to_array_recursively();
+//
+//
+//$proprietario = new Proprietario($contato, 'Jose da Silva');
+//
+//
+//$imovel = new Imovel('Casa grande com varanda',
+//    new Endereco('77006667', 'Quadra 404 Norte Al 10 N 30'),
+//    $proprietario,
+//    $tipo_imovel[1],
+//    $tipo_negocio[1],
+//    680
+//);
+
+
+//$porcaria_de_namespace = $GLOBALS['ROOT_PROJECT_PATH'] . '/apps/tools/twig_obtect_to_template.php';
+//require_once($porcaria_de_namespace);
+
+
+$view = new TwigViewBase(null, $use_cache = false);
+
+$view->render_by_parans('index.twig', $tipo_imovel->to_array_recursively());
+/*
+$view->name = 'Krzysztof';
+$view->friends = array(
+    array(
+        'firstname' => 'John',
+        'lastname' => 'Smith'
+    ),
+    array(
+        'firstname' => 'Britney',
+        'lastname' => 'Spears'
+    ),
+    array(
+        'firstname' => 'Brad',
+        'lastname' => 'Pitt'
+    )
+);
+$view->vaca = 'Muuuuu';
+
+//$view->render('hello.twig', $params);
+*/
+
